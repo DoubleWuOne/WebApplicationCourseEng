0331948dc7287b0bc332f554e8efe5a7a1640303
Jakub Pietraszkiewicz <kubronis@gmail.com>
2022-11-04 17:33:54 +0100

    end of section 3

--- WebApplicationCourseEng/Startup.cs
+++ WebApplicationCourseEng/Startup.cs
@@ -35,10 +35,11 @@ namespace WebApplicationCourseEng
             });
             
             services.AddControllers();
-       /*     services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplicationCourseEng", Version = "v1" });
-            }); */
+            services.AddCors();
+            /*     services.AddSwaggerGen(c =>
+                 {
+                     c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplicationCourseEng", Version = "v1" });
+                 }); */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +55,8 @@ namespace WebApplicationCourseEng
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
